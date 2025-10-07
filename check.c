c168a0e90921233ed0d3abe847d063284ba73733
Yuria Kumura <ria@Yurias-MacBook-Air.local>
2025-10-07 11:00:54 +0900

    changed check

--- check.c
+++ check.c
@@ -3,9 +3,22 @@
 #include "struct_headers.h"
 
 
-void check_horizontal(boardObject *game_board, int player, int ){
-    for(int a = 0; a<4; a++){
-        if(tilePlacement)
+int check_horizontal(boardObject *game_board, int x, int y){
+    int count = 0;
+
+    if(game_board->board[x][y] == (game_board->board[x-1][y]||game_board->board[x+1][y])){
+        if(game_board->board[x-1][y] && game_board->board[x+1][y]){
+            count += 3;
+            if(game_board->board[x-2][y] || game_board->board[x+2][y]){
+                count += 1;
+            }
+        } else {
+            count += 2;
+        }
+        
+    }
+    if (count == 4){
+        
     }
 
 }
