21e00c4dcb4269bc4a871d3ddeaabeafe798cb09
Yuria Kumura <ria@Yurias-MacBook-Air.local>
2025-10-07 12:04:52 +0900

    updated check

--- check.c
+++ check.c
@@ -2,94 +2,117 @@
 #include <stdio.h>
 #include "struct_headers.h"
 
-
 int check_horizontal(boardObject *game_board, int x, int y){
+    char testCase[4] = {'X', 'X', 'X', 'X'};
     int count = 0;
-    //if the newest placement is left-most column
-    if(x == 0){
-        for(int a = 1; a<4; a++){
-            if(game_board->board[x][y] == game_board->board[x+a][y]){
+    //checking each "4" block
+    for(int a = 0; a<4; a++){
+        //checking each position
+        for(int b = 0; b<4; b++){
+            if(game_board->board[a+b][y] == testCase[b]){
                 count++;
-            } else {
-                //doesn't win this round
-                return 0;
+                if(count == 4){
+                    //win this round
+                    break;
+                }
             }
-        }
-    }
-
-    //if the newest placement is right-most column
-    if(x == game_board->cols-1){
-                for(int a = 1; a<4; a++){
-            if(game_board->board[x][y] == game_board->board[x-a][y]){
-                count++;
-            } else {
-                //doesn't win this round
-                return 0;
-            }
-        }
-    } 
-    
-    //if the placement is anywhere else in the row
-    else if(game_board->board[x][y] == (game_board->board[x-1][y]||game_board->board[x+1][y])){
-        if(game_board->board[x-1][y] && game_board->board[x+1][y]){
-            count += 3;
-            if(game_board->board[x-2][y] || game_board->board[x+2][y]){
-                count += 1;
-            }
-        } else {
-            count += 2;
+        
         }
         
     }
-    if (count == 4){
-        //you win this round
-        return 1;
-    }
-    
 
 }
+// int check_horizontal(boardObject *game_board, int x, int y){
+//     int count = 0;
+//     //if the newest placement is left-most column
+//     if(x == 0){
+//         for(int a = 1; a<4; a++){
+//             if(game_board->board[x][y] == game_board->board[x+a][y]){
+//                 count++;
+//             } else {
+//                 //doesn't win this round
+//                 return 0;
+//             }
+//         }
+//     }
 
-
-int check_vertical(boardObject *game_board, int x, int y){
-    int count = 0;
-    //if the newest placement is left-most column
-    if(y == 0){
-        for(int a = 1; a<4; a++){
-            if(game_board->board[x][y] == game_board->board[x][y+a]){
-                count++;
-            } else {
-                //doesn't win this round
-                return 0;
-            }
-        }
-    }
-
-    //if the newest placement is right-most column
-    if(y == game_board->rows-1){
-                for(int a = 1; a<4; a++){
-            if(game_board->board[x][y] == game_board->board[x][y-a]){
-                count++;
-            } else {
-                //doesn't win this round
-                return 0;
-            }
-        }
-    } 
+//     //if the newest placement is right-most column
+//     if(x == game_board->cols-1){
+//                 for(int a = 1; a<4; a++){
+//             if(game_board->board[x][y] == game_board->board[x-a][y]){
+//                 count++;
+//             } else {
+//                 //doesn't win this round
+//                 return 0;
+//             }
+//         }
+//     } 
     
-    //if the placement is anywhere else in the row
-    else if(game_board->board[x][y] == (game_board->board[x][y-1]||game_board->board[x][y+1])){
-        if(game_board->board[x][y-1] && game_board->board[x][y+1]){
-            count += 3;
-            if(game_board->board[x][y-2] || game_board->board[x][y+2]){
-                count += 1;
-            }
-        } else {
-            count += 2;
-        }
+//     //if the placement is anywhere else in the row
+//     else if(game_board->board[x][y] == (game_board->board[x-1][y]||game_board->board[x+1][y])){
+//         if(game_board->board[x-1][y] && game_board->board[x+1][y]){
+//             count += 3;
+//             if(game_board->board[x-2][y] || game_board->board[x+2][y]){
+//                 count += 1;
+//             }
+//         } else {
+//             count += 2;
+//         }
         
-    }
-    if (count == 4){
-        //you win this round
-        return 1;
-    }
-}
+//     }
+//     if (count == 4){
+//         //you win this round
+//         return 1;
+//     }
+    
+
+// }
+
+
+// int check_vertical(boardObject *game_board, int x, int y){
+//     int count = 0;
+//     //if the newest placement is top-most column
+//     if(y == 0){
+//         for(int a = 1; a<4; a++){
+//             if(game_board->board[x][y] == game_board->board[x][y+a]){
+//                 count++;
+//             } else {
+//                 //doesn't win this round
+//                 return 0;
+//             }
+//         }
+//     }
+
+//     //if the newest placement is bottom-most column
+//     if(y == game_board->rows-1){
+//                 for(int a = 1; a<4; a++){
+//             if(game_board->board[x][y] == game_board->board[x][y-a]){
+//                 count++;
+//             } else {
+//                 //doesn't win this round
+//                 return 0;
+//             }
+//         }
+//     } 
+    
+//     //if the placement is anywhere else in the row
+//     else if(game_board->board[x][y] == (game_board->board[x][y-1]||game_board->board[x][y+1])){
+//         if(game_board->board[x][y-1] && game_board->board[x][y+1]){
+//             count += 3;
+//             if(game_board->board[x][y-2] || game_board->board[x][y+2]){
+//                 count += 1;
+//             }
+//         } else {
+//             count += 2;
+//         }
+        
+//     }
+//     if (count == 4){
+//         //you win this round
+//         return 1;
+//     }
+// }
+
+// check_diagonal(boardObject *game_board, int x, int y){
+//     //if the newest placement is bottom
+// }
