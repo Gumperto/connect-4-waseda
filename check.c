824a6d1ddc2897374b5ca8d1a1a57645096848a5
Yuria Kumura <ria@Yurias-MacBook-Air.local>
2025-10-07 11:22:38 +0900

    updated check

--- check.c
+++ check.c
@@ -5,7 +5,6 @@
 
 int check_horizontal(boardObject *game_board, int x, int y){
     int count = 0;
-
     //if the newest placement is left-most column
     if(x == 0){
         for(int a = 1; a<4; a++){
@@ -44,4 +43,46 @@ int check_horizontal(boardObject *game_board, int x, int y){
         //you win this round
     }
 
+}
+
+
+int check_vertical(boardObject *game_board, int x, int y){
+    int count = 0;
+    //if the newest placement is left-most column
+    if(y == 0){
+        for(int a = 1; a<4; a++){
+            if(game_board->board[x][y] == game_board->board[x][y+a]){
+                count++;
+            } else {
+                //doesn't win this round
+            }
+        }
+    }
+
+    //if the newest placement is right-most column
+    if(y == game_board->rows-1){
+                for(int a = 1; a<4; a++){
+            if(game_board->board[x][y] == game_board->board[x][y-a]){
+                count++;
+            } else {
+                //doesn't win this round
+            }
+        }
+    } 
+    
+    //if the placement is anywhere else in the row
+    else if(game_board->board[x][y] == (game_board->board[x][y-1]||game_board->board[x][y+1])){
+        if(game_board->board[x][y-1] && game_board->board[x][y+1]){
+            count += 3;
+            if(game_board->board[x][y-2] || game_board->board[x][y+2]){
+                count += 1;
+            }
+        } else {
+            count += 2;
+        }
+        
+    }
+    if (count == 4){
+        //you win this round
+    }
 }
