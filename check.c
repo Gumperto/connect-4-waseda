990f0a398e1460cece7ff8cc8964ba765af75686
Yuria Kumura <ria@Yurias-MacBook-Air.local>
2025-10-07 10:27:32 +0900

    Add check'

---
+++ check.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "struct_headers.h"
+
+void check_placement(int player, int columnInput){
+    if(columnInput <= 7){
+
+    } else {
+        printf("Invalid column\n");
+    }
+
+}
