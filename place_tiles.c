2352e72ca4b5d9bbd625050c09a17957763b6927
valerie <valerie@ruri.waseda.jp>
2025-10-07 10:52:21 +0900

    struct_headers.h

--- place_tiles.c
+++ place_tiles.c
@@ -2,18 +2,22 @@
 #include <stdlib.h>
 #include "struct_headers.h"
 
-void placeTiles(boardObject *game_board, int columnN, int player) {
+coordinate *placeTiles(boardObject *game_board, int columnN, int player) {
+    coordinate *p_coordinate;
+    p_coordinate->y = columnN;
     if(player == 1) {
-        for(int i = game_board->rows - 1; i > 0; i--) {
+        for(int i = game_board->rows - 1; i >= 0; i--) {
             if(game_board->board[i][columnN] == '0') {
                 game_board->board[i][columnN] = 'X';
             }
         }
     } else {
-        for(int i = game_board->rows - 1; i > 0; i--) {
+        for(int i = game_board->rows - 1; i >= 0; i--) {
             if(game_board->board[i][columnN] == '0') {
                 game_board->board[i][columnN] = 'O';
+                p_coordinate->x = i;
             }
         }
-    }    
-};
+    }
+    return p_coordinate;
+}
