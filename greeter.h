590b079e15cd033a3d86bc9ac9dc7a671555569a
gumperto <162938203+Gumperto@users.noreply.github.com>
2025-10-07 16:46:08 +0900

    Refactor: put long prints into greeter method

---
+++ greeter.h
@@ -0,0 +1,6 @@
+#ifndef GREETER
+#define GREETER
+
+extern void greeter(char* player1, char* player2);
+
+#endif
--- main.c
+++ main.c
@@ -1,32 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "struct_headers.h"
+#include "greeter.h"
 #define MAX_ROWS 6
 #define MAX_COLS 7
 #define PLAYER_1 1
 #define PLAYER_2 2
+#define MAX_NAME_SIZE 128
 
 int main() {
-   char player1[20], player2[20];
-   int player1_choice, player2_choice;
-   int player1_index, player2_index;
+   char* player1 = malloc(sizeof(char) * MAX_NAME_SIZE);
+   char* player2 = malloc(sizeof(char) * MAX_NAME_SIZE);
+
+   greeter(player1, player2);
 
    coordinate* recent_coords = malloc(sizeof(coordinate));
-
    boardObject* game_board = create_board(MAX_ROWS,MAX_COLS);
    fill_board(game_board);
 
-   printf("===Welcome to Connect 4!===\n");
-
-   printf("What's Player 1's name? >>> ");
-   scanf("%s", player1);
-   printf("Hello, %s! Your tile is: X\n", player1);
-
-   printf("What's Player 2's name? >>> ");
-   scanf("%s", player2);
-   printf("Hello, %s! Your tile is: O\n", player2);
-
-   // Add driver logic based on pseudocode
    while (1) {
       printf("Player %s choice: ", player1);
       scanTiles(game_board, PLAYER_1);
@@ -41,4 +32,6 @@ int main() {
 
    free_board(game_board);
    free(recent_coords);
+   free(player1);
+   free(player2);
 }
