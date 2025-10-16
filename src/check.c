a8559db3aec2c7529f40ec8292233c44fed11d66
yriak2428 <yuria.kumura@gmail.com>
2025-10-17 00:08:04 +0900

    reverted back check.c, no more bools

--- src/check.c
+++ src/check.c
@@ -1,9 +1,7 @@
 #include "struct_headers.h"
 #include "macros.h"
 #define WIN_NUMBER 4
-#include <stdbool.h>
 #include <stdlib.h>
-
 
 int check_horizontal(boardObject *game_board, int x, int y, int player);
 int check_vertical(boardObject *game_board, int x, int y, int player);
@@ -11,17 +9,11 @@ int check_diag_left(boardObject *game_board, int x, int y, int player);
 int check_diag_right(boardObject *game_board, int x, int y, int player);
 
 int check_connect_4(boardObject* game_board, int x, int y, playerData* player) {
-    winType* win_type = (winType*)malloc(sizeof(winType));
-    win_type->horizontal = false;
-    win_type->vertical = false;
-    win_type->diag_left = false;
-    win_type->diag_right = false;
-    
-    if(check_horizontal(game_board, x, y, player->player_id) >= WIN_NUMBER) win_type->horizontal = true;
-    if(check_vertical(game_board, x, y, player->player_id) >= WIN_NUMBER) win_type->vertical = true;
-    if(check_diag_left(game_board, x, y, player->player_id) >= WIN_NUMBER) win_type->diag_left = true;
-    if(check_diag_right(game_board, x, y, player->player_id) >= WIN_NUMBER) win_type->diag_right = true;
-    if (win_type->horizontal == true || win_type->vertical == true || win_type->diag_left == true || win_type->diag_right == true) return 1;
+    if(
+        check_horizontal(game_board, x, y, player->player_id) >= WIN_NUMBER || 
+        check_vertical(game_board, x, y, player->player_id) >= WIN_NUMBER ||  
+        check_diag_left(game_board, x, y, player->player_id) >= WIN_NUMBER || 
+        check_diag_right(game_board, x, y, player->player_id) >= WIN_NUMBER ) return 1;
     return 0;
 }
 
@@ -34,12 +26,15 @@ int check_horizontal(boardObject *game_board, int x, int y, int player){
         increment_char = OPPONENT_SYMBOL;
 
     int count = 0;
-    // Run left
     int runner = x;
+    //Run left
     while (runner >= 0) {
-        if (game_board->board[y][runner] != increment_char) break;
-        count++;
-        runner--;
+        if (game_board->board[y][runner] != increment_char){
+            break;
+        } else {
+            count++;
+            runner--;
+        }
     }
 
     // Run right
